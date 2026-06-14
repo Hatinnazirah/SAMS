3a623a1945d6544a57c5ad6f8dc05cf117ff6fdc
adilahzulkifli <cb23088@adab.umpsa.edu.my>
2026-06-15 02:10:11 +0800

    My curriculum activities code

---
+++ lib/screens/ManageCurriculumCoCurriculumActivities/PusatAdabCurriculumPage.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class PusatAdabCurriculumPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Curriculum Management')),
+      body: Center(child: Text('Pusat Adab Curriculum Page')),
+    );
+  }
+}

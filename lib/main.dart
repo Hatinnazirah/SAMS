ccab848a7d989638eee97ef2520845f036fd64da
adilahzulkifli <cb23088@adab.umpsa.edu.my>
2026-06-18 10:32:27 +0800

    Saved all changes on manage-curriculum-activities

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'firebase_options.dart';
 import 'screens/ManageCurriculumCoCurriculumActivities/StudentCurriculumPage.dart';
 import 'PusatAdabDashboard.dart';
 
-<<<<<<< Updated upstream
-void main() {
-=======
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
   print('✅ Firebase initialized successfully!');
->>>>>>> Stashed changes
   runApp(MyApp());
 }
 
